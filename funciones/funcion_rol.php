100618ab1df71cfb4f125cc8a811014206f64f8d
Eder <edersank@gmail.com>
2023-07-11 23:09:09 -0600

    Funcion para saber el tipo de usuario a traves de la session (almacena el login y determina el tipo de usuario)

--- funciones/funcion_rol.php
+++ funciones/funcion_rol.php
@@ -1,30 +1,19 @@
 <?php
 
-function rol($rol){
-
-	//inicio session
-	session_start();
-	//igualar usuario a dato almacenado en variable global $_SESSION['']
-	$usuario = $_SESSION['login'];
-
-
-	//Definicion, si no hay sesion redirige al login
-	if(!isset($_SESSION['login'])){
-		header('Location: login.php');
-	}
+function rol($login){
 	
 //conexion a bd
 try {
 
 	$conexion = new PDO ('mysql: host=localhost; dbname=almacen', 'root', '');
-	$consulta = $conexion -> prepare("SELECT * FROM usuarios where login = '$usuario' ");
+	$consulta = $conexion -> prepare("SELECT * FROM usuarios WHERE login = '$login' ");
 	$consulta -> execute();
 	$resultado = $consulta -> fetchAll();
 
-	foreach ($resultado as $fila);
-
-	$idrol = $fila['id_rol'];
-
+	foreach ($resultado as $fila) {
+		$idrol = $fila['id_rol'];	
+	}
+	
 	switch ($idrol) {
 		case 1:
 			 $rol = 'admin';
