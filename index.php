13bcf4ca80ee46850c31d73a4ffbbf7227831cbc
Eder <edersank@gmail.com>
2023-07-12 23:29:27 -0600

    Cambio por plantilla css

--- index.php
+++ index.php
@@ -8,6 +8,8 @@ $login = login($login_s);
 include('funciones/funcion_rol.php');
 $rol_s = rol($login);
 
+include('funciones/funcion_nombre_login.php');
+$nombre_s = nombre($login);
 
 require('views/index.view.php')
 
