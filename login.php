a66b96acc937656a58fbef0ec5594692fb801163
Eder <edersank@gmail.com>
2023-07-15 00:47:51 -0600

    Se agrego control en bitacora agrega el dato login a bd

--- login.php
+++ login.php
@@ -29,6 +29,22 @@ if($_SERVER['REQUEST_METHOD'] == 'POST'){
 	$resultado = $consulta -> fetch();
 
 	if($resultado !== false) {
+
+		$consulta_u = $conexion -> prepare("SELECT * FROM usuarios WHERE login = '$usuario'");
+		$consulta_u -> execute();
+		$resultado2 = $consulta_u -> fetchAll();
+
+
+		date_default_timezone_set('America/Mexico_City');
+		$hoy = date('Y-m-d h:i:s');
+
+		foreach ($resultado2 as $fila) {
+			$id = $fila['id_usuario'];
+		}
+
+		$consulta_bitacora = $conexion -> prepare ("INSERT INTO bitacora_login VALUES (null, '$id', '$hoy', 'Login')");
+		$consulta_bitacora -> execute();
+
 		$_SESSION['login'] = $usuario;
 
 		header('Location: index.php');
