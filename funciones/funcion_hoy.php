bb942a1b298a78d10e20f3b1a102eee044b3628c
Eder <edersank@gmail.com>
2023-07-15 00:44:02 -0600

    Nuevas funciones para reducir codigo

---
+++ funciones/funcion_hoy.php
@@ -0,0 +1,12 @@
+<?php
+
+
+function hoy() {
+date_default_timezone_set('America/Mexico_City');
+$fecha = date('d-m-Y');
+
+return $fecha;
+
+}
+
+?>
---
+++ funciones/funcion_img_login.php
@@ -0,0 +1,26 @@
+<?php
+
+
+function img($login){
+	
+//conexion a bd
+try {
+
+	$conexion = new PDO ('mysql: host=localhost; dbname=almacen', 'root', '');
+	$consulta = $conexion -> prepare("SELECT * FROM usuarios WHERE login = '$login' ");
+	$consulta -> execute();
+	$resultado = $consulta -> fetchAll();
+
+	foreach ($resultado as $fila){
+		$img = $fila['img_usuario'];	
+	}	
+	
+} catch (PDOException $e) {
+	echo 'Error ' . $e->getMessage();
+}
+
+return $img;
+
+}
+
+?>
