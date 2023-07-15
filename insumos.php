3f7b0e582f295de1188fd327a29358a4c6d165c7
Eder <edersank@gmail.com>
2023-07-15 00:49:31 -0600

    Se modifico por uso de funciones

--- insumos.php
+++ insumos.php
@@ -1,15 +1,19 @@
 <?php
 
-$login_s = '';
-
 include('funciones/funcion_login.php');
-$login = login($login_s);
+$login = login();
 
 include('funciones/funcion_rol.php');
 $rol_s = rol($login);
 
 include('funciones/funcion_nombre_login.php');
 $nombre_s = nombre($login);
+
+include('funciones/funcion_img_login.php');
+$img_s = img($login);
+
+include('funciones/funcion_hoy.php');
+$hoy = hoy();
 
 /*
 try{
