c8df8a425fbbd49c56f438da00028013adc021d6
Eder <edersank@gmail.com>
2023-07-15 00:50:15 -0600

    Se modifico por falla de seguridad

--- surtir_insumo.php
+++ surtir_insumo.php
@@ -1,18 +1,30 @@
 <?php
 
-try {
+include('funciones/funcion_login.php');
+$login = login();
 
-$id_insumo = $_GET['id_insumo'];
+include('funciones/funcion_rol.php');
+$rol_s = rol($login);
 
-$conexion = new PDO('mysql: host=localhost; dbname=almacen', 'root', '');
-$consulta_preparada = $conexion -> prepare("SELECT * FROM insumos WHERE id_insumo = '$id_insumo'");
-$consulta_preparada -> execute();
-$resultado = $consulta_preparada -> fetchAll();
+if($rol_s == 'admin' OR $rol_s == 'almacen'){
+	try {
 
-	
-} catch (PDOException $e) {
-	echo "Error" . $e -> getMessage();
+	$id_insumo = $_GET['id_insumo'];
+
+	$conexion = new PDO('mysql: host=localhost; dbname=almacen', 'root', '');
+	$consulta_preparada = $conexion -> prepare("SELECT * FROM insumos WHERE id_insumo = '$id_insumo'");
+	$consulta_preparada -> execute();
+	$resultado = $consulta_preparada -> fetchAll();
+
+		
+	} catch (PDOException $e) {
+		echo "Error" . $e -> getMessage();
+	}	
+}else{
+	echo 'El usuario no tiene cumple con las credenciales';
+	header ('Location: buscar.php');
 }
+
 
 require ('views\surtir_insumo.view.php');
 
