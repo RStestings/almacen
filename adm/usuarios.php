3453e853a17684a5a7fc55ac0d0ea56a67a10fe6
Eder <edersank@gmail.com>
2023-07-21 15:46:39 -0600

    Para ver usuarios del sistema

--- adm/usuarios.php
+++ adm/usuarios.php
@@ -1,17 +1,45 @@
 <?php
 
-try {
+include('../funciones/funcion_login.php');
+$login = login();
 
-$conexion = new PDO ('mysql: host=localhost; dbname=almacen', 'root', '');
+include('../funciones/funcion_rol.php');
+$rol_s = rol($login);
+
+include('../funciones/funcion_nombre_login.php');
+$nombre_s = nombre($login);
+
+include('../funciones/funcion_img_login.php');
+$img_s = img($login);
+
+include('../funciones/funcion_hoy.php');
+$hoy = hoy();
+
+include('../funciones/funcion_conexion.php');
+$conexion = fconexion();
+
+
+if ($conexion != false){
 $consulta = $conexion -> prepare ('SELECT * FROM usuarios');
 $consulta -> execute();
 $resultado = $consulta -> fetchAll();
-
-	
-} catch (PDOException $e) {
-	echo 'Error: ' . $e -> getMessage();
 }
 
-require ('views/usuarios.view.php');
+$_POST['buscar'] = isset($_POST['buscar']) ? $_POST['buscar'] : false;
+
+if (empty($buscar)){
+		$buscar= '%' . $_POST['buscar'] . '%';
+		
+
+		$consulta_preparada = $conexion -> prepare("SELECT * FROM usuarios WHERE nombre LIKE '$buscar' ");
+		$consulta_preparada -> execute();
+		$resultado = $consulta_preparada -> fetchAll();
+	}
+
+if($rol_s != 'admin'){
+	echo 'Crear pagina de usuarios restringido';
+}else{
+	require ('views/usuarios.view.php');
+}
 
 ?>
