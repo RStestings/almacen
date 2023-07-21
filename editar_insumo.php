0ec3af1913698d677e52e5d4c34d843017c190b9
Eder <edersank@gmail.com>
2023-07-21 15:49:40 -0600

    Se agregan funciones y vista

--- editar_insumo.php
+++ editar_insumo.php
@@ -6,12 +6,24 @@ $login = login();
 include('funciones/funcion_rol.php');
 $rol_s = rol($login);
 
+include('funciones/funcion_nombre_login.php');
+$nombre_s = nombre($login);
+
+include('funciones/funcion_img_login.php');
+$img_s = img($login);
+
+include('funciones/funcion_hoy.php');
+$hoy = hoy();
+
+include('funciones/funcion_conexion.php');
+$conexion = fconexion();
+
+
 if($rol_s == 'admin'){
 	try {
 
 	$id_insumo = $_GET['id_insumo'];
 
-	$conexion = new PDO('mysql: host=localhost; dbname=almacen', 'root', '');
 	$consulta_preparada = $conexion -> prepare("SELECT * FROM insumos WHERE id_insumo = '$id_insumo'");
 	$consulta_preparada -> execute();
 	$resultado = $consulta_preparada -> fetchAll();
